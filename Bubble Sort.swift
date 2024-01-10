30a5834d3ff9d38beb04e2ba1de34206e2c9f5ae
imrishuj <65715489+imrishuj@users.noreply.github.com>
2024-01-10 17:02:10 +0530

    Recursive bubble sort

--- Bubble Sort.swift
+++ Bubble Sort.swift
@@ -22,3 +22,25 @@ func doBubbleSort() {
 print("Array before sort", array)
 doBubbleSort()
 print("Array after sort", array)
+
+func doRecursiveBubbleSort(_ array: inout [Int], _ length: Int) {
+    guard length > 1 else { return }
+    var swapped = false
+    for secondIndex in 0..<(length-1) {
+        if array[secondIndex] > array[secondIndex + 1] {
+            swapped = true
+            let temp = array[secondIndex]
+            array[secondIndex] = array[secondIndex + 1]
+            array[secondIndex + 1] = temp
+        }
+    }
+    if !swapped {
+        return
+    }
+    doRecursiveBubbleSort(&array, (length-1))
+}
+
+array = [7, 2, 9, 5, 1]
+print("Array before recursive sort", array)
+doRecursiveBubbleSort(&array, array.count)
+print("Array after recursive sort", array)
