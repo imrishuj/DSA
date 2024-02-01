5a5cab58897bcdfa64abf9cae46f0721086b51da
imrishuj <65715489+imrishuj@users.noreply.github.com>
2024-02-01 19:20:42 +0530

    Next Permutation

---
+++ Array/Next Permutation.swift
@@ -0,0 +1,42 @@
+// Next Permutation : find next lexicographically greater permutation
+
+var arr = [1, 2, 3, 6, 5, 4]
+
+func nextPermutationusingOptimalApproach() {
+    var pivotIndex = -1
+    var index = arr.count-2
+    
+    while(index >= 0) {
+        if arr[index] < arr[index + 1] {
+            pivotIndex = index
+            break
+        }
+        index -= 1
+    }
+    
+    if pivotIndex == -1 {
+        return
+    } else {
+    
+        var index = arr.count-1
+        while(index > pivotIndex) {
+            if arr[index] > arr[pivotIndex] {
+                arr.swapAt(index, pivotIndex)
+                index -= 1
+                break
+            }
+        }
+
+        var start = pivotIndex + 1
+        var end = arr.count - 1
+        while(start < end) {
+            arr.swapAt(start, end)
+            start += 1
+            end -= 1
+        }
+    }
+    print("Next Permutation of array using optimal approach", arr)
+}
+nextPermutationusingOptimalApproach()
+
+
